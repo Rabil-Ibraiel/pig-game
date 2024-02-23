c2dfae36321f98b34a35b7a5a29dffd80fe83b14
Rabil <rabilabrail@gmail.com>
2024-02-23 16:21:12 +0300

    update logic game

--- script.js
+++ script.js
@@ -1,46 +1,61 @@
 "use strict";
 
-const playerOne = document.getElementById("current--0");
-const playerTow = document.getElementById("current--1");
+const current0 = document.getElementById("current--0");
+const current1 = document.getElementById("current--1");
+const score0 = document.getElementById("score--0");
+const score1 = document.getElementById("score--1");
+let playerTurn = document.querySelector(".player--active").classList[1];
+
 let img = document.querySelector(".dice");
 img.style.display = "none";
-let playerTurn = document.querySelector(".player--active").classList[1];
+
+let total = 0;
+let playerOneScore = 0;
+let playerTwoScore = 0;
 
 document.querySelector(".btn--roll").addEventListener("click", () => {
   let randNumber = Math.floor(Math.random() * 6 + 1);
   img.style.display = "block";
   img.src = `https://raw.githubusercontent.com/Rabil-Ibraiel/pig-game/main/images/dice-${randNumber}.png`;
 
-  let total = 0;
-
   if (randNumber == 1) {
-    howIsPlaying()[0].innerHTML = 0;
-    howIsPlaying()[1].innerHTML = 0;
+    total = 0;
+    if (playerTurn == "player--0") {
+      playerOneScore = 0;
+      current0.textContent = 0;
+    } else {
+      playerTwoScore = 0;
+      current1.textContent = 0;
+    }
     nextPlayerTurn();
   } else {
-    total += randNumber;
+    total = randNumber;
     score(total);
   }
 });
 
 document.querySelector(".btn--hold").addEventListener("click", () => {
   if (playerTurn == "player--0") {
-    document.getElementById("score--0").innerHTML = playerOne.innerHTML;
+    score0.textContent = playerOneScore;
     nextPlayerTurn();
   } else {
-    document.getElementById("score--1").innerHTML = playerTow.innerHTML;
+    score1.textContent = playerTwoScore;
     nextPlayerTurn();
   }
+  total = 0;
 });
 
 function score(total) {
   if (playerTurn == "player--0") {
-    playerOne.innerHTML = Number(playerOne.innerHTML) + Number(total);
-    winCheck(playerOne.innerHTML);
+    playerOneScore += total;
+    current0.textContent = playerOneScore;
+    winCheck(playerOneScore);
   } else {
-    playerTow.innerHTML = Number(playerTow.innerHTML) + Number(total);
-    winCheck(playerTow.innerHTML);
+    playerTwoScore += total;
+    current1.textContent = playerTwoScore;
+    winCheck(playerTwoScore);
   }
+  total = 0;
 }
 
 function nextPlayerTurn() {
@@ -58,15 +73,16 @@ function nextPlayerTurn() {
 }
 
 function newGame() {
-  playerOne.innerHTML = 0;
-  playerTow.innerHTML = 0;
-  document.getElementById("score--0").innerHTML = 0;
-  document.getElementById("score--1").innerHTML = 0;
+  current0.innerHTML = 0;
+  current1.innerHTML = 0;
+  score0.innerHTML = 0;
+  score1.innerHTML = 0;
+  total = 0;
+  playerOneScore = 0;
+  playerTwoScore = 0;
   if (playerTurn == "player--1") {
     nextPlayerTurn();
   }
-
-  // Countrine here
 }
 
 document.querySelector(".btn--new").addEventListener("click", newGame);
@@ -80,22 +96,14 @@ function winCheck(total) {
     });
 
     if (playerTurn == "player--0") {
-      document.getElementById("score--0").innerHTML = total;
+      score0.innerHTML = total;
     } else {
-      document.getElementById("score--1").innerHTML = total;
+      score1.innerHTML = total;
     }
 
     document.querySelector(".player--winner").style.display = "flex";
     document.getElementById("winner-name").innerHTML = `${
       playerTurn == "player--0" ? "Player 1" : "Player 2"
     } Win the game!`;
-  }
-}
-
-function howIsPlaying() {
-  if (playerTurn == "player--0") {
-    return [document.getElementById("score--0"), playerOne];
-  } else {
-    return [document.getElementById("score--1"), playerTow];
   }
 }
